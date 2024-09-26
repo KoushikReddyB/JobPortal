83359e32f82fe74263f0db18ad94a659df1b6571
KoushikReddyB <2200090018@kluniversity.in>
2024-09-26 11:30:15 +0530

    Created Spring boot project

---
+++ JobPortalLogin/src/main/java/dev/zap/JobPortalLogin/JobPortalLoginApplication.java
@@ -0,0 +1,13 @@
+package dev.zap.JobPortalLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalLoginApplication.class, args);
+	}
+
+}
---
+++ JobPortalLogin/src/test/java/dev/zap/JobPortalLogin/JobPortalLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zap.JobPortalLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
