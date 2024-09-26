eee2c95f99f767a8a3692dd50bd3179419b91159
KoushikReddyB <2200090018@kluniversity.in>
2024-09-26 12:28:23 +0530

    fixing bugs

--- JobPortalLogin/src/main/java/dev/zap/JobPortal/registration/RegistrationService.java
+++ JobPortalLogin/src/main/java/dev/zap/JobPortal/registration/RegistrationService.java
@@ -1,5 +1,8 @@
 package dev.zap.JobPortal.registration;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegistrationService {
 	public String register(RegistrationRequest request) {
 		return "Works!";
