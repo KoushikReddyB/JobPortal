f7a4aef3c96f33e273629e983d056ea8bddbf715
KoushikReddyB <2200090018@kluniversity.in>
2024-09-26 11:57:45 +0530

    Created new pacakage called register

---
+++ JobPortalLogin/src/main/java/dev/zap/JobPortal/registration/RegistrationController.java
@@ -0,0 +1,14 @@
+package dev.zap.JobPortal.registration;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping(path = "api/v1/registration")
+@AllArgsConstructor
+
+public class RegistrationController {
+
+}
